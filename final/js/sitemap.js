59860f146ca7e20ba59679d999ab6542f10a692e
Jayson Fong <jaysonf1007@gmail.com>
2019-11-26 11:11:25 -0500

    Navigation Adjustments & Search

---
+++ final/js/sitemap.js
@@ -0,0 +1,5 @@
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 88) {
+        window.location.replace("https://gtc.jaysonfong.us/cist1510/final/sitemap.xml");
+    }
+});
